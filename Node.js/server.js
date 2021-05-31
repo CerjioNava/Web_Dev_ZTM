ea2f08e10fa593870d10a9bc43da73fe36b3b3a1
unknown <cerjionava@gmail.com>
2021-05-31 16:03:42 -0400

    Adding new stuff, learning RESTful APIsgit add .!

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,3 +1,5 @@
+// TODO LO COMENTADO AL INICIO FUE HECHO SIN EXPRESS.JS
+/*
 //const http = require('http'); 								// Usamos el modulo "http" de Node.
 import http from 'http';
 
@@ -11,7 +13,7 @@ const server = http.createServer( (request, response) => {		// Creamos un servid
 		name: 'John',
 		hobby: 'Skating'
 	};
-
+ 
 	//response.setHeader('Content-Type', 'text/html');			// Describimos el tipo de contenido vamos a enviar. HTML en este caso.
 	//response.end('<h1>Hellooooo</h1>')						// Contenido (?)
 
@@ -21,3 +23,42 @@ const server = http.createServer( (request, response) => {		// Creamos un servid
 });
 
 server.listen(3000);										// Le otorgamos el puerto donde escuchará. 'http://localhost:3000/'
+*/
+
+//--------------------------------------------------------------------------------------------------------
+
+// AHORA USANDO EXPRESS.JS para crear un servidor.
+
+import express, { response } from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));	// Middleware (body-parser ya no)
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send("Getting root");					// Enviando JSON
+});
+app.get('/profile', (req, res) => {
+	res.send("Getting profile");				// Enviando JSON
+});
+app.post('/profile', (req, res) => {		// Esto funciona si realmente se envía data.
+	
+	// Para que esto funcione, usamos un middleware.
+	console.log(req.body);					// Si queremos acceder a la información enviada en postman.
+	res.send("Success");							// Enviando JSON
+
+	//res.send("Hellooo");					// Se convierte automáticamente a HTML
+	//res.send("<h1>Hellooo</h1>");			// Envío de HTML
+	/*
+	const user = {
+		name: 'Sally',
+		hobby: 'Soccer'
+	}
+	res.send(user);
+	*/
+})
+
+
+app.listen(3000);
+
